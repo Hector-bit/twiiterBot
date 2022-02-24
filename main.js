55ddb90ee8dd56f5f6af9a32233e535cf926be94
Hector-bit <hectormartinez.2978@gmail.com>
2022-02-24 10:44:08 -0800

    retweets once a day

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-const rwClient = require("./twitterClient.js");
+const rwClient = require("./twitterClient");
+const CronJob = require("cron").CronJob;
 
 const tweet = async () => {
     try {
-        await rwClient.v2.tweet("good morning #techtwitter, from #helperbot")
+        await rwClient.v2.tweet("good morning #techtwitter, from #twitter_api_v2")
     } catch(e) {
         console.error(e)
     }
 }
 
-tweet()
+const jsTweets = async () => {
+    try {
+        let data = await rwClient.v2.search("100daysofcode", {'media.fields': 'url' });
+        const thingy = data.tweets[0]
+        console.log(thingy.id, 'thingy id')
+        const user_info = await rwClient.v2.me();
+        console.log(user_info.data.id, 'user id')
+        await rwClient.v2.retweet( `${user_info.data.id}`, `${thingy.id}`)
+        return data.tweets[0]
+    } catch(e){
+        console.error(e)
+    }
+}
+
+
+const job = new CronJob("0 5 * * *", () => {
+    // tweet()
+    jsTweets
+})
+
+// job.start();
+
+// jsTweets();
+
+
