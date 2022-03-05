44ed121a97f9ea6ee78d0bf610bdad297b143c19
Hector-bit <hectormartinez.2978@gmail.com>
2022-03-05 08:24:14 -0800

    main update

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 const rwClient = require("./twitterClient");
 const CronJob = require("cron").CronJob;
 
+const mytwitterID = '1486239064663597061';
+
 const tweet = async () => {
     try {
         await rwClient.v2.tweet("good morning #techtwitter, from #twitter_api_v2")
     } catch(e) {
         console.error(e)
     }
+}
+
+const like_ten_tweets = async () => {
+    //search for tweets
+    let tweets = await rwClient.v2.search("100daysofcode", {'media.fields': 'url'})
+    let uniqueIDS = new Set();
+    for (const tweet of tweets){
+        uniqueIDS.add(tweet.id);
+    }
+    console.log(uniqueIDS)
+
+    uniqueIDS.forEach(element => {
+        console.log("liking this tweet: ", element);
+        rwClient.v2.like(mytwitterID, element);
+        setTimeout( () => {console.log("waiting")}, 1500)
+    });
+    // console.log(mytwitterID, uniqueIDS[0])
+    // await rwClient.v2.like(mytwitterID ,uniqueIDS[0]);
 }
 
 const jsTweets = async () => {
@@ -24,13 +44,15 @@ const jsTweets = async () => {
 }
 
 
-const job = new CronJob("0 5 * * *", () => {
-    // tweet()
-    jsTweets()
-})
+// const job = new CronJob("0 5 * * *", () => {
+//     // tweet()
+//     jsTweets()
+// })
 
-job.start();
+// job.start();
 
-// jsTweets();
+
+like_ten_tweets();
+jsTweets();
 
 
